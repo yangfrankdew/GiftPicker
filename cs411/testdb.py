897afa55f87a3123101a1f4689bfecd0016872a3
yangfrankdew <57195505+yangfrankdew@users.noreply.github.com>
2020-08-04 14:55:19 -0500

    Add files via upload

---
+++ cs411/testdb.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from django.http import HttpResponse
+
+from TestModel.models import Test
+
+
+# 数据库操作
+def testdb(request):
+    # 修改其中一个id=1的name字段，再save，相当于SQL中的UPDATE
+    test1 = Test.objects.get(id=1)
+    test1.name = 'Google'
+    test1.save()
+
+    # 另外一种方式
+    # Test.objects.filter(id=1).update(name='Google')
+
+    # 修改所有的列
+    # Test.objects.all().update(name='Google')
+
+    return HttpResponse("<p>修改成功</p>")# -*- coding: utf-8 -*-
+
+from django.http import HttpResponse
+
+from TestModel.models import Test
+
+# 数据库操作
+def testdb(request):
+    # 修改其中一个id=1的name字段，再save，相当于SQL中的UPDATE
+    test1 = Test.objects.get(id=1)
+    test1.name = 'Google'
+    test1.save()
+
+    # 另外一种方式
+    #Test.objects.filter(id=1).update(name='Google')
+
+    # 修改所有的列
+    # Test.objects.all().update(name='Google')
+
+    return HttpResponse("<p>修改成功</p>")
